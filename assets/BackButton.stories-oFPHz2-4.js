3f26b34cf3e845134448bac6d346e1ef2737c784
atmin <atmin@users.noreply.github.com>
2026-05-09 14:06:58 +0000

    deploy: atmin/atmin.net@9b9bc239c6473da0f04ec3fce781ec23c181558f

--- assets/BackButton.stories-oFPHz2-4.js
+++
@@ -1,5 +0,0 @@
-import{B as e}from"./BackButton-B3JYRzRI.js";import"./iframe-CZa_wsSi.js";import"./preload-helper-PPVm8Dsz.js";import"./createLucideIcon-DmIffzK0.js";const m={title:"Shell/BackButton",component:e},t={},r={args:{to:"/settings"}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:"{}",...t.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    to: '/settings'
-  }
-}`,...r.parameters?.docs?.source}}};const n=["Default","CustomTarget"];export{r as CustomTarget,t as Default,n as __namedExportsOrder,m as default};
