de7be8a6caa891dd3d4fe3d5f0145c7ad015cc30
atmin <atmin@users.noreply.github.com>
2026-06-20 14:59:47 +0000

    deploy: atmin/atmin.net@dc5d74ccbc60de037f61760d86ec19dccf3f4509

---
+++ assets/formatter-EIJCOSYU-B3PzC8ub.js
@@ -0,0 +1,1 @@
+import{d as m,r as e}from"./DocsRenderer-LL677BLK-_lM65Ae3.js";import{g as o}from"./iframe-MyHRwHZG.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-D8lRTNGn.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
