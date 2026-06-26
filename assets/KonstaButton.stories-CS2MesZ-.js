c3e6da9a699fda657b8983a214b1516b55526b9e
atmin <atmin@users.noreply.github.com>
2026-06-26 09:21:43 +0000

    deploy: atmin/atmin.net@0eddda648bab1471a360fbd60f1edd3d454e3ba4

---
+++ assets/KonstaButton.stories-CS2MesZ-.js
@@ -0,0 +1,13 @@
+import{B as e,j as t,a as o,b as r}from"./iframe-Bg5_a6e4.js";import"./preload-helper-PPVm8Dsz.js";const a={title:"Konsta/Harness Check",component:e,parameters:{layout:"fullscreen"}},n={render:()=>t.jsxs(t.Fragment,{children:[t.jsx(o,{children:"Buttons"}),t.jsxs(r,{strong:!0,className:"space-y-4",children:[t.jsx(e,{children:"Filled"}),t.jsx(e,{tonal:!0,children:"Tonal"}),t.jsx(e,{outline:!0,children:"Outline"}),t.jsx(e,{clear:!0,children:"Clear"}),t.jsx(e,{rounded:!0,children:"Rounded"}),t.jsx(e,{disabled:!0,children:"Disabled"})]})]})};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  render: () => <>
+            <BlockTitle>Buttons</BlockTitle>
+            <Block strong className="space-y-4">
+                <Button>Filled</Button>
+                <Button tonal>Tonal</Button>
+                <Button outline>Outline</Button>
+                <Button clear>Clear</Button>
+                <Button rounded>Rounded</Button>
+                <Button disabled>Disabled</Button>
+            </Block>
+        </>
+}`,...n.parameters?.docs?.source}}};const u=["Buttons"];export{n as Buttons,u as __namedExportsOrder,a as default};
